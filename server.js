cc122bf4720848d95189c10a36c0a2157d2d6fe3
Peter Szweryn <peter.szweryn@gmail.com>
2017-01-05 07:52:47 +0100

    Npm init, package install, added .env to .gitignore, declared vars and loaded needed packages in server.js, declared schema and connected to db in server.js

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
 // vim: set tabstop=4 softtabstop=4 shiftwidth=4 expandtab:
+
+// needed for process.env stuff
+require('dotenv').config();
+
+// vars from process.env
+var dbURL = process.env.DB_URL
+var googleAPIKey = process.env.API_KEY
+var googleCX = process.env.CX
+var googleAppID = process.env.APP_ID
+
+// load needed packages
+var express = require('express');
+var mongoose = require('mongoose');
+var request = require('request');
+
+// load own modules
+var api = require('./api/image-search.js');
+var routes = require('./routes/index.js');
+
+// init app
+var app = express();
+
+// connect mongoose
+// db START
+
+mongoose.connect(dbURL);
+
+// define latestSearches schema
+var latestSearchesSchema = new mongoose.Schema({
+    term: String,
+    when: Date
+});
+
+var latestSearches = mongoose.model('latestSearches', latestSearchesSchema);
+
+// db END
