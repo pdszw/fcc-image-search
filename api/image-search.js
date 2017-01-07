a5f1643164a24daa1b664729b299d1e47f911c56
Peter Szweryn <peter.szweryn@gmail.com>
2017-01-07 16:54:57 +0100

    testing with dummy data

--- api/image-search.js
+++ api/image-search.js
@@ -1,4 +1,14 @@
 var request = require('request');
 
-exports.submitSearch = function(query,offset) {
+// pass to this function the search terms (query) and offset (defaults to 1 if nothing passed).
+exports.submit = function(terms,offset = 1) {
+    return new Promise (function(resolve,reject) {
+
+        function testing() {
+            var searchResult = { terms: terms, offset: offset };
+            resolve(searchResult);        // resolve the promise with var 'searchResult', which then returns it to whatever called submitSearches.
+        }
+        //return { terms: terms, offset: offset }
+        setTimeout(testing, 5000);
+    });
 };
--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,34 @@ var imageSearch = require('../api/image-search.js');
 
 var Searches = require('../models/searches.js');
 
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// GET /api/imagesearch/:query(*)
-
-router.get('/api/imagesearch/:query(*)', function(req,res) {
-    var query = req.params.query;
-    res.send(query);
+// GET /api/imagesearch/:terms?offset=1
+// eg: /api/imagesearch/lolcats%20funny?offset=10
+router.get('/api/imagesearch/:terms', function(req,res) {
+    imageSearch.submit(req.params.terms,req.query.offset)
+    .then(
+        function(result) { 
+            res.json(result); 
+        }
+    ); 
 });
 
 // GET /api/latest/imagesearch/
 router.get('/api/latest/imagesearch/', function(req,res) {
-    res.send('latest');
+//      THIS WILL ACTUALLY NEED TO BE 'Searches.find().sort(-when).limit(10).then(function...
+//    imageSearch.latest().then(function(result) { res.json(result) } );
+//    res.json(result);
+
+        function testing2() {
+            var latestResult = { "row": [ { terms: "something", when: "2 days ago" }, { terms: "something else", when: "3 days ago" } ] };
+            res.json(latestResult);
+        }
+        setTimeout(testing2, 3000);
+
 });
 
 module.exports = router;
