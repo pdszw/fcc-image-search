faab81b6254045fe3b3d1ebf00e52b2d49c20975
Peter Szweryn <peter.szweryn@gmail.com>
2017-01-08 10:30:18 +0100

    Got search working only with sample data (ie. not with actually call to CSE), search history saves to db, can retrieve last 10 (or less if not yet 10) searches from db.

--- api/image-search.js
+++ api/image-search.js
@@ -1,14 +1,98 @@
+'use strict';
+
 var request = require('request');
+var Searches = require('../models/searches.js'); 
 
 // pass to this function the search terms (query) and offset (defaults to 1 if nothing passed).
 exports.submit = function(terms,offset = 1) {
     return new Promise (function(resolve,reject) {
+        var result;
 
+/*
         function testing() {
             var searchResult = { terms: terms, offset: offset };
             resolve(searchResult);        // resolve the promise with var 'searchResult', which then returns it to whatever called submitSearches.
         }
         //return { terms: terms, offset: offset }
         setTimeout(testing, 5000);
+*/
+
+        var reformattedTerms = terms.split(' ').join('+');
+
+        var start;
+        if (typeof offset === 'undefined') { 
+            start = ""; 
+        } else {
+            start = "&start=" + offset;
+        }
+
+        // GET https://www.googleapis.com/customsearch/v1?q=testing+1+2+3&cx=015210821254511219620%3Amqlrkwto5u0&num=10&searchType=image&key={YOUR_API_KEY}
+        var query = `https://www.googleapis.com/customsearch/v1?`
+            + `searchType=image`
+            + `${start}`
+            + `&num=10`
+            + `&q=${reformattedTerms}`
+            + `&cx=${process.env.CX}`
+            + `&key=${process.env.API_KEY}`;
+
+        var fs = require('fs');
+        var path = require('path');
+        var data = JSON.parse(fs.readFileSync(path.join(__dirname, 'sample.json')));
+
+/*
+        request(query, {json:true}, function(err,res,data) {
+
+            if (err) {
+                console.log('error: ' + err);
+                result = { error: err }; 
+                resolve(result);
+            } else if (!err && res.statusCode == 200) {
+*/
+                result = data.items.map(function(item) {
+                    return {
+                        url: item.link,
+                        snippet: item.snippet,
+                        thumbnail: item.image.thumbnailLink,
+                        context: item.image.contextLink
+                    };
+                });                
+
+                Searches.create({
+                    term: terms,
+                    when: Date.now() 
+                }, function(err,doc) {
+                    if (err) {
+                        console.log('error: ' + err);
+                    } else {
+                        console.log('saved query to db. term: ' + terms);
+                    }
+                });
+
+                resolve(result);
+/*
+            } else {
+                console.log('something went wrong.');
+            }
+
+        });
+*/
     });
 };
+
+/*
+exports.latest = function() {
+    return new Promise(function(resolve,reject) {
+        Searches.find().select('term when').sort({"when": -1}).limit(10)
+        .then(
+            function(result) {
+                var resultLatest = {
+                    term: result.term,
+                    when: result.when
+                }
+                resolve(resultLatest);
+            }
+        );
+    });
+
+};
+*/
--- routes/index.js
+++ routes/index.js
@@ -24,15 +24,28 @@ router.get('/api/imagesearch/:terms', function(req,res) {
 // GET /api/latest/imagesearch/
 router.get('/api/latest/imagesearch/', function(req,res) {
 //      THIS WILL ACTUALLY NEED TO BE 'Searches.find().sort(-when).limit(10).then(function...
-//    imageSearch.latest().then(function(result) { res.json(result) } );
-//    res.json(result);
 
+
+        // select only term and when fields, explicitly exclude _id field
+        Searches.find().select('-_id term when').sort({"when": -1}).limit(10)
+        .then(
+          function(result) {
+              res.json(result);
+          }
+        );
+
+
+
+/*
+        imageSearch.latest().then(function(result) { res.json(result) });
+*/
+/*
         function testing2() {
             var latestResult = { "row": [ { terms: "something", when: "2 days ago" }, { terms: "something else", when: "3 days ago" } ] };
             res.json(latestResult);
         }
         setTimeout(testing2, 3000);
-
+*/
 });
 
 module.exports = router;
